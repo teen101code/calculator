e4e1a9bfe027df52e4a8bbfbb3a6a64a53c0ce22
Tannu <tannusharma01012002@gmail.com>
2023-06-16 16:50:45 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,38 @@
+let display= document.getElementById('display');
+let button= Array.from(document.getElementsByClassName('buttons'));
+
+button.map (buttons => {
+    buttons.addEventListener('click', (e) => {
+        switch(e.target.innerText)
+        {
+            case 'C': display.innerText="";
+                      break;
+            case '->':
+                  if(display.innerText)
+                  {
+
+                      display.innerText=display.innerText.slice(0,-1);
+                  }
+                  break;
+            case '=':
+                try{
+                    display.innerText=eval( display.innerText);
+
+                  }
+                 catch{
+                    display.innerText='error!';
+                  }
+                
+            break;
+        
+
+
+
+        default:
+            display.innerText += e.target.innerText;
+
+        }
+    }
+    
+    );
+});
